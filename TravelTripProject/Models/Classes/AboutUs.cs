c653d76c6f7cb803b253812ea5aaf3a734aa1f16
heavenssx <119794002+Bulakdilann@users.noreply.github.com>
2023-02-28 15:04:35 +0300

    Classes and database were created.Add project files.

---
+++ TravelTripProject/Models/Classes/AboutUs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Classes
+{
+    public class AboutUs
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Explanation { get; set; }
+    }
+}
---
+++ TravelTripProject/Models/Classes/Comments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Classes
+{
+    public class Comments
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+
+        // relationships
+        public Blog Blog { get; set; }
+       
+    }
+}
