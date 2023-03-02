321cafc35843d895baf3b74592168e0a5d51e9e8
heavenssx <119794002+Bulakdilann@users.noreply.github.com>
2023-03-02 13:01:03 +0300

    Asp.Net Mvc5 ile Tatil Seyahat Sitesi Blog Sayfası

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -10,16 +10,24 @@ namespace TravelTripProject.Controllers
     public class BlogController : Controller
     {
         Context _db = new Context();
+        BlogComment bc = new BlogComment();
         public ActionResult Index()
         {
-            var bloglar = _db.Blogs.ToList();
-            return View(bloglar);
+            //var bloglar = _db.Blogs.ToList();
+            bc.Deger1 = _db.Blogs.ToList();
+            bc.Deger3 = _db.Blogs.OrderByDescending(x=>x.Id).Take(3).ToList();
+            return View(bc);
         }
 
+       
         public ActionResult BlogDetail(int id)
         {
-            var blogbul = _db.Blogs.Where(x => x.Id == id).ToList();
-            return View(blogbul);
+
+            // var blogbul = _db.Blogs.Where(x => x.Id == id).ToList();
+            bc.Deger1 = _db.Blogs.Where(x => x.Id == id).ToList();
+            bc.Deger2 = _db.Comments.Where(x => x.BlogId == id).ToList();
+            bc.Deger3 = _db.Blogs.OrderByDescending(x => x.Id).Take(3).ToList();
+            return View(bc);
         }
     }
 }
