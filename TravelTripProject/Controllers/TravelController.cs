2c8c1e2b7b2a34767ded6e7e37bd6e91a89d5b3c
heavenssx <119794002+Bulakdilann@users.noreply.github.com>
2023-03-02 15:52:09 +0300

    Asp.Net Mvc5 ile Tatil Seyahat Sitesi Ana Sayfa

--- TravelTripProject/Controllers/TravelController.cs
+++ TravelTripProject/Controllers/TravelController.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Models.Classes;
 
 namespace TravelTripProject.Controllers
 {
     public class TravelController : Controller
     {
         // GET: Travel
+        Context _db = new Context();
         public ActionResult Index()
         {
-            return View();
+            var listele = _db.Blogs.Take(4).ToList(); //Slider içine Ana sayfada 4 resim gelsin
+            return View(listele);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = _db.Blogs.OrderBy(x => x.Id).Take(3).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var degerler = _db.Blogs.OrderByDescending(x => x.Id).Take(10).ToList();
+            return PartialView(degerler);
         }
     }
 }
